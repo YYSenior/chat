4116e885b46752640887a46027a17e755f329511
悦怿 <89171553+YYSenior@users.noreply.github.com>
2023-04-02 14:56:50 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 本站功能强大，响应速度快，欢迎自部署。
+- 本站默认使用@悦怿学长私有账户API，你可以填入自己的 openai_api_key 以使用个人模型。
+- 本站依托于Github开源项目及Vercel创建，详情请移至YYSenior频道访问开源作者学习部署步骤。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
