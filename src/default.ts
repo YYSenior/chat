cef2ba1770b06fac38ebc6e071cc8dc057a9ed79
悦怿 <89171553+YYSenior@users.noreply.github.com>
2023-04-05 22:59:56 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -1,12 +1,14 @@
 export const defaultSetting = {
   continuousDialogue: true,
-  archiveSession: false,
+  archiveSession: true,
   openaiAPIKey: "",
   openaiAPITemperature: 60,
   systemRule: ""
 }
 
 export const defaultMessage = `
-- 本站默认使用@悦怿学长私有账户API，你可以填入自己的 openai_api_key 以使用个人模型。
-- 本站依托于Github开源项目及Vercel创建，详情请移至YYSenior频道访问开源作者学习部署步骤。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 本站默认使用@悦怿学长私有账户API
+- 你可以填入openai_api_key，使用个人模型
+- 本站依托于Github开源项目创建，详情请访问YYSenior/Chat频道
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行
+- 开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设`
